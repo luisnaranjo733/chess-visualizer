0b8ed0b9240f5407ee461b91934807af037d6ce8
Luis Naranjo <luisnaranjo733@gmail.com>
2017-04-13 11:05:11 -0700

    added spinner during loading

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import 'whatwg-fetch'
+import 'whatwg-fetch';
+import _ from 'lodash';
 
 class Header extends Component {
   render() {
     return (
-      <h1>Chess visualizer</h1>
+      <h1>LiChess visualizer</h1>
     );
   }
 }
@@ -23,14 +24,59 @@ class UsernameForm extends Component {
   }
 }
 
+function DelayPromise(delay) {  
+  //return a function that accepts a single variable
+  return function(data) {
+    //this function returns a promise.
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        //a promise that is resolved after "delay" milliseconds with the data provided
+        console.log(`Waiting for ${delay} ms`);
+        resolve(data);
+      }, delay);
+    });
+  }
+}
+
+let Spinner = (props) => {
+  const loading = props.loading;
+  if (loading) {
+    return (
+      <div id="spinner">
+        <i className="fa fa-spinner fa-spin fa-2x" aria-hidden="true"></i>
+      </div>
+    );
+  } else {
+    return (
+      <div id="spinner"></div>
+    );
+  }
+};
+
+
+/*class Spinner extends Component {
+  render() {
+    let style = {
+      color: 'red'
+    }
+
+    return (
+      <div id="spinner">
+        <i className="fa fa-spinner fa-spin fa-2x" aria-hidden="true"></i>
+      </div>
+    );
+  }
+}*/
+
 export default class HomePage extends Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
       username: 'jnaranj0',
-      page: 1,
-      response: null
+      page: 69,
+      games: [],
+      loading: false,
     }
   }
 
@@ -38,20 +84,33 @@ export default class HomePage extends Component {
     this.setState({ username: event.target.value });
   }
 
+  getNextPage = () => {
+    if (this.state.page != null) {
+      let url = `https://en.lichess.org/api/user/${this.state.username}/games?with_opening=1&page=${this.state.page}`;
+      let outerThis = this;
+      
+      fetch(url)
+        .then(function (response) {
+          return response.text();
+        }).then(function (json) {
+          json = JSON.parse(json);
+          let games = _.concat(outerThis.state.games, json.currentPageResults)
+          outerThis.setState({ games: games, page: json.nextPage });  
+        }).then(DelayPromise(1000)).then(() => {
+          this.getNextPage();
+        });
+    } else {
+      console.log('Finished paging through data!');
+      // Disable loading spinner
+      this.setState({loading: false});
+    }
+
+  }
+
   onSubmit = () => {
-    let url = `https://en.lichess.org/api/user/${this.state.username}/games?with_opening=1&page=${this.state.page}`;
-    console.log(url);
-
-    let outerThis = this;
-
-    fetch(url)
-      .then(function (response) {
-        return response.text();
-      }).then(function (json) {
-        json = JSON.parse(json);
-        outerThis.setState({response: json});
-        console.log(json);
-      })
+    this.setState({loading: true});
+    this.getNextPage()
+    // enable loading spinner
   }
 
 
@@ -60,7 +119,9 @@ export default class HomePage extends Component {
       <div className='container'>
         <Header />
         <p>Url: {this.state.username}</p>
-        {/*<pre>{this.state.response}</pre>*/}
+        <p>Next page: {this.state.page}</p>
+        <p>Games: {this.state.games.length}</p>
+        <Spinner loading={this.state.loading}/>
         <UsernameForm onUsernameChanged={this.onUsernameChanged} onSubmit={this.onSubmit} />
       </div>
     );
